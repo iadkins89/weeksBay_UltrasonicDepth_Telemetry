fd7541401e0f6e79872e672bb5da1d6db0be593a
Ian Adkins <ian.adkins1989@gmail.com>
2024-11-01 15:03:07 -0500

    Forgot to undo localhost mode

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from server import create_server
 server = create_server()
 app = create_app(server)
 
-if __name__ == '__main__':
-    server.run(port=8080, debug=False, threaded=True)
+#if __name__ == '__main__':
+#    server.run(port=8080, debug=False, threaded=True)
